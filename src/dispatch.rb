27586a94b18ea78b3bf8e71604be96ee19bda503
julie <69617566+juliejgiles@users.noreply.github.com>
2020-09-27 22:16:41 +1000

    Add files via upload
    Update Card and Deck objects and dispatch.rb file to show the rules of the game when the user selects the associated action.

---
+++ src/dispatch.rb
@@ -0,0 +1,34 @@
+require_relative 'controllers/controller.rb'
+
+def dispatch(cmd, params = nil)
+    case cmd
+    when '1'
+        ::RulesController.show
+        
+        # print "\n Rules of the game:"
+        #     print "\n 1) You will be dealt 5 random cards out of a standard deck of 52 cards which have been shuffled by the dealer."
+        #     print "\n 2) Dealer (computer) will reveal 3 cards."
+        #     print "\n 3) You have the option of placing a bet or folding at this stage."
+        #     print "\n 4) If you choose to bet, the dealer will continue to reveal 2 more cards."
+        #     print "\n 5) Player with highest combo wins the game."
+        #     print "\n 6) Player with lowest combo loses a finger.
+        #     \n"
+
+    when '2'
+        # ::Models.Player_hand
+        # print "\n You have been dealt the following cards:
+        # \n"
+        # print ::Deck.Cards[0]
+
+  
+
+    when '3'
+        puts "3"
+  
+    else
+        puts "\n Not a valid option, please try again. 
+        \n"
+        
+    end 
+
+end 
